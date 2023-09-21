6c7e82c2ab58c97a779c2fe4d7b530935d6febd9
Austin Chick <53247428+aschen-builder@users.noreply.github.com>
2023-09-21 07:00:11 -0700

    Update build script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+sudo apt-get install wget qemu-user-static xz-utils -y -q -f
+
+! command -v packer && (
+  echo "packer not found"
+  exit 1
+)
+
 sudo -E packer init .
 sudo -E packer validate .
 sudo -E packer build .
